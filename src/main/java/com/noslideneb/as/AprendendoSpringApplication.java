5af1291edbab6820e036ff429c3f358f88c671fe
benedilson.lima <benedilson.lima@WST-001023.infosolo.com.br>
2018-01-22 14:05:16 -0200

    Teste vendo se da certo

--- src/main/java/com/noslideneb/as/AprendendoSpringApplication.java
+++ src/main/java/com/noslideneb/as/AprendendoSpringApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AprendendoSpringApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AprendendoSpringApplication.class, args);
 	}
